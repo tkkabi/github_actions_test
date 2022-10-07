fcca36c6539e2f24ad2ba6d40610d435ddf4c04c
tk <tapaskabinitr@gmail.com>
2022-10-07 11:27:33 +0100

    x99

---
+++ data-service/tag.py
@@ -0,0 +1,15 @@
+import os
+from datetime import datetime as dt
+env_file = os.getenv('GITHUB_ENV')
+path  = os.getcwd()
+print(path)
+
+version_path  = f'{os.path.dirname(path)}/github_actions_test/.github/workflows/version.txt'
+print(version_path)
+x=dt.now()
+with open(version_path, "r") as f:
+  line  = f.readline()
+
+with open(env_file, "a") as myfile:
+  myfile.write(f"my_date={x}\n")
+  myfile.write(f"my_var={line}")
