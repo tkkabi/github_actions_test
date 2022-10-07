e780d0aafcf8a85377c0da32d6e3adcf557338eb
tk <tapaskabinitr@gmail.com>
2022-10-07 12:21:20 +0100

    x102

--- data-service/tag.py
+++ data-service/tag.py
@@ -4,7 +4,7 @@ env_file = os.getenv('GITHUB_ENV')
 path  = os.getcwd()
 print(path)
 
-version_path  = f'{os.path.dirname(path)}/github_actions_test/.github/workflows/version.txt'
+version_path  = f'{os.path.dirname(path)}/github_actions_test/data-service/version.txt'
 print(version_path)
 x=dt.now()
 with open(version_path, "r") as f:
